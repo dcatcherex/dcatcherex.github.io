60822c35cf570e00d7b96624e49204a3bd496ffb
dcatcherex <dcatcherex@yahoo.com>
2022-02-17 13:45:24 +0700

    update

---
+++ excercise tracker/script/jone.js
@@ -0,0 +1,3 @@
+function getAlert(){
+    alert('Congrat');
+}
---
+++ intro-to-js/jone.js
@@ -0,0 +1,12 @@
+function askMyName(){
+    const name = prompt("ชื่ออะไรเอ่ย");
+    
+    if (name != null) {
+        document.getElementById("myname").innerHTML = "My name is " + name;
+    }
+}
+
+function helloWorld(){
+    const h1message = document.getElementById("myname").innerHTML;
+    alert(h1message + "งานท่วมหัว ทำตัว slowlife");
+}
